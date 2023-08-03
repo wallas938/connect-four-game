def51bffba68733ee584134b03d1e9781d967856
wallas938 <drame.sissako001@gmail.com>
2023-08-03 16:09:06 +0200

    view game mobile tablet and desktop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RulesComponent } from './pages/rules/rules.component';
 import { HomeComponent } from './pages/home/home.component';
+import { GameComponent } from './pages/game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RulesComponent,
-    HomeComponent
+    HomeComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
